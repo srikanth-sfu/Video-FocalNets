3a579e9794ce0f88d3711a437f53b90ab28674ec
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-07-24 18:55:56 -0400

    debug

--- scripts/hmdb51/vit_base.sh
+++ scripts/hmdb51/vit_base.sh
@@ -1,5 +1,7 @@
 python -m torch.distributed.launch --nproc_per_node 4 main.py \
 --cfg configs/hmdb51/vit_base.yaml
+echo $?
+echo "Crossed here"
 if [ $? -eq 124 ]; then
   echo "The script timed out after ${MAX_HOURS} hour(s). Restarting..."
   # Call the script itself again with the same configuration
