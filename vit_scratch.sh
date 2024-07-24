14b460b20cfde4e416bc4b705b7270ed9b974459
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-07-24 18:35:53 -0400

    debug

--- vit_scratch.sh
+++ vit_scratch.sh
@@ -16,6 +16,7 @@ source activate focal
 #bash scripts/hmdb51/video-focalnet_base.sh
 # Check if the timeout command's exit status is 124, which indicates a timeout occurred
 echo $?
+echo "Crossed this path"
 bash scripts/hmdb51/vit_base.sh
 echo $?
 if [ $? -eq 124 ]; then
