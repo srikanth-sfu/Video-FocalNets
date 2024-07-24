4eaa61ee2dd994a43d429fd5b73cc228815ee175
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-07-24 17:47:30 -0400

    expire code debug

--- vit_scratch.sh
+++ vit_scratch.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem-per-cpu=64G 
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:v100l:4
-#SBATCH --time=0-03:00
+#SBATCH --time=0-00:01
 #SBATCH -o /home/smuralid/error/slurm-%j.out  # Write the log on scratch
 #SBATCH -e /home/smuralid/error/slurm-%j.err
 
@@ -16,6 +16,7 @@ source activate focal
 #bash scripts/hmdb51/video-focalnet_base.sh
 # Check if the timeout command's exit status is 124, which indicates a timeout occurred
 bash scripts/hmdb51/vit_base_scratch.sh
+echo $?
 if [ $? -eq 124 ]; then
   echo "The script timed out after ${MAX_HOURS} hour(s). Restarting..."
   # Call the script itself again with the same configuration
