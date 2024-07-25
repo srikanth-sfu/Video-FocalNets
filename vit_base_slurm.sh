6b76ea45fde91534744ca5490338db3a7e6be7cb
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-07-25 17:09:19 -0400

    rename base slurm script

--- vit_base_slurm.sh
+++ vit_base_slurm.sh
@@ -3,8 +3,8 @@
 #SBATCH --account=def-mpederso
 #SBATCH --mem-per-cpu=64G 
 #SBATCH --nodes=1
-##SBATCH --gres=gpu:4
-#SBATCH --time=0-00:02
+#SBATCH --gres=gpu:4
+#SBATCH --time=0-03:00
 #SBATCH --open-mode=append
 #SBATCH -o /home/smuralid/error/slurm-%j.out  # Write the log on scratch
 #SBATCH -e /home/smuralid/error/slurm-%j.err
@@ -16,7 +16,7 @@ source /home/smuralid/anaconda3/bin/activate
 source activate focal
 
 # Check if the timeout command's exit status is 124, which indicates a timeout occurred
-timeout 5s bash scripts/hmdb51/vit_base.sh
+timeout 179m bash scripts/hmdb51/vit_base.sh
 
 if [ $? -eq 124 ]; then
   echo "The script timed out after ${MAX_HOURS} hour(s). Restarting..."
