63822684daf79c57ed9fcf2929ebcd48fa8e113e
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-08-07 21:38:59 -0400

    alter numnodes

--- scripts/ssv2/vit_base.sh
+++ scripts/ssv2/vit_base.sh
@@ -1,2 +1,2 @@
-python -m torch.distributed.launch --nnodes=4 --nproc-per-node=4 main.py \
+python -m torch.distributed.launch --nnodes=3 --nproc-per-node=4 main.py \
 --cfg configs/ssv2/vit_base.yaml
