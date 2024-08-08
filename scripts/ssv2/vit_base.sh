565454b6f64c33dfe64e2d5e5bf3e30390643801
Srikanth Muralidharan <smuralid@cedar1.int.cedar.computecanada.ca>
2024-08-07 20:11:15 -0700

    latest scripts

--- scripts/ssv2/vit_base.sh
+++ scripts/ssv2/vit_base.sh
@@ -1,2 +1,3 @@
-python -m torch.distributed.launch --nnodes=3 --nproc-per-node=4 main.py \
---cfg configs/ssv2/vit_base.yaml
+#python -m torch.distributed.launch --nnodes=3 --nproc-per-node=4 main.py \
+#--cfg configs/ssv2/vit_base.yaml
+torchrun --nproc_per_node=4 --master_port=29600 --nnodes=$SLURM_NNODES --node_rank=$SLURM_NODEID main.py --cfg configs/ssv2/vit_base.yaml
--- vit_base_ssv2_slurm.sh
+++ vit_base_ssv2_slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=vit_train
 #SBATCH --account=rrg-mpederso
 #SBATCH --mem-per-cpu=64G 
-#SBATCH --nodes=3
+#SBATCH --nodes=2
 #SBATCH --gres=gpu:p100l:4
 #SBATCH --time=0-03:00
 #SBATCH --open-mode=append
@@ -16,8 +16,7 @@ source /home/smuralid/anaconda3/bin/activate
 source activate focal
 
 # Check if the timeout command's exit status is 124, which indicates a timeout occurred
-timeout 179m bash scripts/ssv2/vit_base.sh
-
+timeout 179m bash scripts/ssv2/vit_base.sh 
 if [ $? -eq 124 ]; then
   echo "The script timed out after ${MAX_HOURS} hour(s). Restarting..."
   # Call the script itself again with the same configuration
